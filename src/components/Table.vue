4447e78c6c039635ad2acf298cb5f7bc36a643e8
Gary Grumbley <ggrumbley@gmail.com>
2018-09-03 19:47:45 -0700

    Add basic table component

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>I am a table!</div>
+  <div class="table-row">
+    <span class="large-column">
+      <a href="#">title</a>
+    </span>
+    <span class="mid-column">author</span>
+    <span class="small-column">comments</span>
+    <span class="small-column">points</span>
+    <span class="small-column">
+      <button class="button-inline">Dismiss</button>
+    </span>
+  </div>
 </template>
 
 <script>
@@ -10,5 +20,49 @@ export default {
 
 
 <style lang="scss" scoped>
+  .table {
+    margin: 20px 0;
+  }
+  .table-header {
+    display: flex;
+    line-height: 24px;
+    font-size: 16px;
+    padding: 0 10px;
+    justify-content: space-between;
 
+    > span {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      padding: 0 5px;
+    }
+  }
+  .table-empty {
+    margin: 200px;
+    text-align: center;
+    font-size: 16px;
+  }
+  .table-row {
+    display: flex;
+    line-height: 24px;
+    white-space: nowrap;
+    margin: 10px 0;
+    padding: 10px;
+    background: #ffffff;
+    border: 1px solid #e3e3e3;
+
+    > span {
+      overflow: hidden;
+      text-overflow: ellipsis;
+      padding: 0 5px;
+    }
+  }
+  .large-column {
+    width: 40%;
+  }
+  .mid-column {
+    width: 30%;
+  }
+  .small-column {
+    width: 10%;
+  }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,7 +5,7 @@
   </div>
   <Table />
   <div class="interactions">
-    <Button />
+    <button>More</button>
   </div>
 </div>
 </template>
@@ -13,7 +13,6 @@
 <script>
 
 import HelloWorld from '@/components/HelloWorld.vue';
-import Button from '@/components/Button.vue';
 import Search from '@/components/Search.vue';
 import Table from '@/components/Table.vue';
 
@@ -21,53 +20,18 @@ export default {
   name: 'home',
   components: {
     HelloWorld,
-    Button,
     Search,
     Table,
   },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
   .page {
     margin: 20px;
   }
   .interactions {
     text-align: center;
-  }
-  .table {
-    margin: 20px 0;
-  }
-  .table-header {
-    display: flex;
-    line-height: 24px;
-    font-size: 16px;
-    padding: 0 10px;
-    justify-content: space-between;
-  }
-  .table-empty {
-    margin: 200px;
-    text-align: center;
-    font-size: 16px;
-  }
-  .table-row {
-    display: flex;
-    line-height: 24px;
-    white-space: nowrap;
-    margin: 10px 0;
-    padding: 10px;
-    background: #ffffff;
-    border: 1px solid #e3e3e3;
-  }
-  .table-header > span {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    padding: 0 5px;
-  }
-  .table-row > span {
-    overflow: hidden;
-    text-overflow: ellipsis;
-    padding: 0 5px;
   }
   .button-inline {
     border-width: 0;
