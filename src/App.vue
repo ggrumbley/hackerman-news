406cd662dd11f7bf2eb9168da59e9769c4f0ee05
Gary Grumbley <ggrumbley@trackforce.com>
2018-09-05 15:40:55 -0700

    Add grid background

--- src/App.vue
+++ src/App.vue
@@ -5,24 +5,56 @@
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
+    <main></main>
+    <div class="grid">
+      <div class="grid-inner">
+        <div class="hori">
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+        </div>
+        <div class="vert">
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+          <div class="line"></div>
+        </div>
+      </div>
+    </div>
   </div>
+
 </template>
 
 <style lang="scss">
   // @import "~/style/_vars.scss";
 
-  body {
+  // body {
     // color: #222;
     // background: #f4f4f4;
     // font: 400 14px CoreSans, Arial,sans-serif;
-    background-color: #000000;
-    margin: 0px;
-    overflow: hidden;
-    background-image: radial-gradient(ellipse farthest-corner at center top, #23233f 0%, #000000 60%);
-    background-image: url(../public/img/arcade.jpg);
-    background-size: cover;
-    background-position: top center;
-  }
+    // background-color: #000000;
+    // margin: 0px;
+    // overflow: hidden;
+    // background-image: radial-gradient(ellipse farthest-corner at center top, #23233f 0%, #000000 60%);
+    // background-image: url(../public/img/arcade.jpg);
+    // background-size: cover;
+    // background-position: top center;
+  // }
 
   a {
     color: #222;
@@ -99,5 +131,191 @@
   .flex-5 {
     flex: 5;
   }
+$speed: 5s;
+$increase-vert: $speed/10;
+$scanlines: 1px;
+
+@keyframes blur-n-hue {
+  0% {filter: blur(10px) hue-rotate(-15deg);}
+  50% {filter: blur(20px) hue-rotate(0deg);}
+  100% {filter: blur(10px) hue-rotate(-15deg);}
+}
+@keyframes move {
+  from {top: 0%; opacity: 0.8;}
+  to {top: 100%; opacity: 1;}
+}
+@keyframes angle {
+  0% {transform: rotateX(58deg);}
+  50% {transform: rotateX(60deg);}
+  100% {transform: rotateX(58deg);}
+}
+@keyframes bg {
+  0% {background-color: rgba(46, 46, 46, 0);}
+  49% {background-color: rgba(46, 46, 46, 0);}
+  50.5% {background-color: rgba(155, 155, 206, .4);}
+  51% {background-color: rgba(46, 46, 46, 0);}
+  51.5% {background-color: rgba(155, 155, 206, .3);}
+  57% {background-color: rgba(46, 46, 46, 0);}
+  100% {background-color: rgba(46, 46, 46, 0);}
+}
+html {
+  background: rgba(46, 46, 46, 1.00);
+}
+body {
+  background: rgba(46, 46, 46, 1.00);
+  animation: bg 10s infinite ease-out;
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
+  margin: 0;
+  &:before {
+    content: "";
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(30,30,30,0.2); //fallback
+    background: -webkit-repeating-linear-gradient(top, transparent 0px, transparent $scanlines, rgba(30,30,30,0.15) $scanlines, rgba(30,30,30,0.15) $scanlines*2);
+    -webkit-background-size: 100% $scanlines*2;
+    background: -moz-repeating-linear-gradient(top, transparent 0px, transparent $scanlines, rgba(30,30,30,0.15) $scanlines, rgba(30,30,30,0.15) $scanlines*2);
+    -moz-background-size: 100% $scanlines*2;
+    display: block;
+    z-index: 10;
+  }
+  // &:after {
+  // 	animation: bg 10s infinite ease-out;
+  // 	content: "";
+  // 	position: absolute;
+  // 	top: 0;
+  // 	left: 0;
+  // 	width: 100%;
+  // 	height: 100%;
+  // 	display: block;
+  // 	z-index: 20;
+  // }
+}
+.text {
+  color: white;
+  font: small-caps lighter 14px/150% "Helvetica Neue", Helvetica, sans-serif;
+  letter-spacing: .2rem;
+  position: absolute;
+  top: 20%;
+  left: 50%;
+  transform: translateX(-50%);
+  text-align: left;
+  line-height: 1.2em;
+  filter: colour-shift(10);
+  z-index: 20;
+  b {
+    font-size: 2.5em;
+    &:after {
+      content: "v2";
+      font-weight: 100;
+      font-size: 12px;
+      position: absolute;
+    }
+  }
+}
+
+main {
+  animation: blur-n-hue $speed infinite ease-in-out;
+  content: "";
+  display: block;
+  background: rgba(224, 87, 90, 1);
+  background: linear-gradient(180deg,
+    rgba(150, 50, 50, 0.8) 0%,
+    rgba(200, 50, 100, 1) 6%,
+    rgba(220,220,220, 1) 8%,
+    rgba(224, 87, 90, 1) 12%,
+    rgba(224, 37, 90, 1) 100%
+  );
+  width: 150vw;
+  height: 80%;
+  left: -25%;
+  bottom: -10%;
+  position: absolute;
+  z-index: -100;
+}
+
+.grid {
+  perspective: 300px;
+  width: 100%;
+  height: 100%;
+  position: relative;
+  z-index: -100;
+
+  .grid-inner {
+    position: absolute;
+    top:0;
+    left:0;
+    right:0;
+    bottom:0;
+    width:100%;
+    height:100%;
+    transform: rotateX(60deg);
+    //animation: angle 5s infinite ease-in-out;
+  }
+}
+.line {
+  display: block;
+  position: absolute;
+  width: 0px;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  border: 1px solid white;
+  box-sizing: border-box;
+  height: 100%;
+  background: white;
+  box-shadow: 0px 0px 6px 2px rgba(255,225,225,0.8);
+  z-index: -100;
+
+}
+.hori, .vert {
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  width: 100vw;
+  height: 100vh;
+  z-index: -100;
+
+}
+.hori {
+  .line {
+    &:nth-child(1) {left: -40%;}
+    &:nth-child(2) {left: -20%;}
+    &:nth-child(3) {left: 0%;}
+    &:nth-child(4) {left: 20%;}
+    &:nth-child(5) {left: 40%;}
+    &:nth-child(6) {left: 60%;}
+    &:nth-child(7) {left: 80%;}
+    &:nth-child(8) {left: 100%;}
+    &:nth-child(9) {left: 120%;}
+    &:nth-child(10) {left: 140%;}
+  }
+}
+
+.vert {
+  .line {
+    animation: move $speed infinite linear;
+    width: 200%;
+    left: -50%;
+    height: 0px;
+    &:nth-child(1) {animation: none; opacity: 0.7;}
+    &:nth-child(2) {animation-delay: $increase-vert;}
+    &:nth-child(3) {animation-delay: $increase-vert*2;}
+    &:nth-child(4) {animation-delay: $increase-vert*3;}
+    &:nth-child(5) {animation-delay: $increase-vert*4;}
+    &:nth-child(6) {animation-delay: $increase-vert*5;}
+    &:nth-child(7) {animation-delay: $increase-vert*6;}
+    &:nth-child(8) {animation-delay: $increase-vert*7;}
+    &:nth-child(9) {animation-delay: $increase-vert*8;}
+    &:nth-child(10) {animation-delay: $increase-vert*9;}
+  }
+}
 
 </style>
